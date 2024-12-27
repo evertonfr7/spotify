40edec47c78e78e54e71724d3e89777c57a9c3fe
Everton <everton7fr@gmail.com>
2024-12-27 16:35:48 -0300

    chore: app title

--- src/components/ui/Logo/index.tsx
+++ src/components/ui/Logo/index.tsx
@@ -1,7 +1,11 @@
+import logo from '../../../assets/logo.svg'
+
 export default function Logo() {
   return (
-    <div className="flex gap-2 items-center">
-      <h1 className="text-2xl font-bold">Spotify</h1>
-    </div>
+    <img
+      src={logo}
+      alt="Spotify Logo"
+      title="Spotify"
+    />
   )
 }
