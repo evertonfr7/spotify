16a87a9f64a322e6746ae07a61e4ab53d830db38
Everton <everton7fr@gmail.com>
2024-12-31 03:33:09 -0300

    feat: offline feedback

--- src/components/layout/BaseNavigation/BaseNavigation.tsx
+++ src/components/layout/BaseNavigation/BaseNavigation.tsx
@@ -1,7 +1,22 @@
+import useOnlineStatus from '@/hooks/useOnlineStatus'
 import { Sidebar } from '../Sidebar/Sidebar'
 import { BaseNavigationProps } from './BaseNavigation.types'
+import Typography from '@/components/ui/Typography'
 
 export function BaseNavigation({ children }: BaseNavigationProps): JSX.Element {
+  const isOnline = useOnlineStatus()
+
+  if (!isOnline) {
+    return (
+      <div className="w-full h-[100dvh] flex justify-between">
+        <Sidebar />
+        <div className="flex justify-center items-center w-full h-screen text-white p-4 md:p-8">
+          <Typography>Você está offline</Typography>
+        </div>
+      </div>
+    )
+  }
+
   return (
     <div className="w-full h-[100dvh] flex justify-between">
       <Sidebar />
--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -1,9 +1,14 @@
 import BaseNavigation from '@/components/layout/BaseNavigation'
+import Typography from '@/components/ui/Typography'
 
 export function Home(): JSX.Element {
   return (
     <BaseNavigation>
-      <></>
+      <div className="flex h-[100vh] flex-col justify-center items-center gap-4">
+        <Typography className="text-white text-lg">
+          Seja bem-vindo ao Spotify Luizalabs Challenge!
+        </Typography>
+      </div>
     </BaseNavigation>
   )
 }
