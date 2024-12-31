8cb3c06b09b6f56fa206e648e6e89a662c07486c
Everton <everton7fr@gmail.com>
2024-12-31 02:45:49 -0300

    feat: add sentry

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { registerSW } from 'virtual:pwa-register'
+import * as Sentry from '@sentry/react'
+
+if (import.meta.env.NODE_ENV === 'production') {
+  Sentry.init({
+    dsn: import.meta.env.VITE_SENTRY_DSN,
+    integrations: [],
+  })
+}
 
 const updateSW = registerSW({
   onNeedRefresh() {
